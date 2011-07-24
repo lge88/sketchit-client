961b666928f12faee800edcbf5b365fe14aa1907
Li Ge <geli19880206@gmail.com>
2011-07-24 01:13:17 -0700

    edit controllers

--- app/controllers/src/all.js
+++ app/controllers/src/all.js
@@ -1,8 +1,23 @@
-sketchit.controllers.sketchitController=Ext.regController("sketchitController", {
-	initAll: function() {
+sketchit.controllers.sketchitController = Ext.regController("sketchitController", {
+	initAll : function() {
 
+		//init views
 		this.buildViews();
-		this.initAllHandlers();
+
+		//init model Root
+		this.Root = new sketchitLib.Root({});
+
+		//init Renderer
+		this.Renderer = new sketchitLib.Renderer({
+			Root : this.Root,
+			Canvas : document.getElementById('workspace')
+		})
+
+		//init event handlers
+		this.initHandlers();
+		
+		//init input stroke array
+		this.inputStrokes = [];
 
 		//init shape recognizer
 		this.shapeRecognizer = new DollarRecognizer();
@@ -10,7 +25,7 @@ sketchit.controllers.sketchitController=Ext.regController("sketchitController", 
 		//init command recognizer
 		//this.commandGenerator = new sketchitLib.commandGenerator();
 	},
-	buildViews: function() {
+	buildViews : function() {
 		this.mainView = this.render({
 			xtype : 'sketchitMain'
 		}, Ext.getBody());
@@ -22,10 +37,11 @@ sketchit.controllers.sketchitController=Ext.regController("sketchitController", 
 		this.bottomBar = this.mainView.getDockedItems()[1];
 
 	},
-	initAllHandlers: function() {
+	
+	initHandlers : function() {
 
 		//init main view event
-		var onOrientationchange= function() {
+		var onOrientationchange = function() {
 			this.resetCanvasPosition()
 		};
 		this.mainView.on({
@@ -33,19 +49,16 @@ sketchit.controllers.sketchitController=Ext.regController("sketchitController", 
 			scope : this
 		})
 		onOrientationchange.call(this);
-		
+
 		this.initCanvasHandlers();
-		
+
 		this.initMenuHandlers();
-		
+
 	},
-	resetCanvasPosition: function(width,height,upleftX,upleftY) {
-		var w= width || this.mainView.getWidth(),
-		h= height || this.mainView.getHeight() -  this.topBar.getHeight() -  this.bottomBar.getHeight(),
-		x= upleftX || 0,
-		y= upleftY || this.topBar.getHeight();
+	resetCanvasPosition : function(width, height, upleftX, upleftY) {
+		var w = width || this.mainView.getWidth(), h = height ||  this.mainView.getHeight() -   this.topBar.getHeight() -   this.bottomBar.getHeight(), x = upleftX || 0, y = upleftY || this.topBar.getHeight();
 		this.canvasWidth = w;
-		this.canvasHeight =  h;
+		this.canvasHeight = h;
 		this.canvasUpLeftX = x;
 		this.canvasUpleftY = y;
 	},
